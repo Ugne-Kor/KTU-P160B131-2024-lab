9d521e16541a8fe5b0d3adb9a9eb44672d213e4f
Ugne-Kor <161746088+Ugne-Kor@users.noreply.github.com>
2024-05-03 19:54:40 +0300

    Update 1-data_prep.R

--- R/1-data_prep.R
+++ R/1-data_prep.R
@@ -1,6 +1,19 @@
+install.packages("tidyverse")
+install.packages("rjson")
+install.packages("jsonlite")
+
 library(tidyverse)
+library(rjson)
+library(jsonlite)
+
 cat("Darbinė direktorija:", getwd())
 download.file("https://atvira.sodra.lt/imones/downloads/2023/monthly-2023.csv.zip", "../data/temp")
 unzip("../data/temp",  exdir = "../data/")
+
+data <- fromJSON("../data/monthly-2023.json")
+data =data %>%
+  filter(`ecoActCode` == '522920') %>%
+  saveRDS("../data/duomenys.rds")
+
 file.remove("../data/temp")
 file.remove("../data/monthly-2023.csv")
