f6290cb703e021954abeaf0261b7b723c82caa19
Ugne-Kor <161746088+Ugne-Kor@users.noreply.github.com>
2024-05-03 21:22:02 +0300

    Update 2-visualization.R

--- R/2-visualization.R
+++ R/2-visualization.R
@@ -1,1 +1,10 @@
 library(tidyverse)
+library(ggplot2)
+
+#2.1 užduotis
+
+data = readRDS("../data/692000.rds")
+png(file="../img/1_Graph.png", width = 1500, height = 1000)
+Histo = hist(data$avgWage, breaks = 200, main = paste("Vidutinio atlyginimo histograma"),
+             xlab = "Atlyginimas", ylab = "Kiekis", col = 'green')
+dev.off()
