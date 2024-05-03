ef81d5683fd862f8c702de9a62140a33115d2a38
Ugne-Kor <161746088+Ugne-Kor@users.noreply.github.com>
2024-05-03 21:25:01 +0300

    Update 2-visualization.R

--- R/2-visualization.R
+++ R/2-visualization.R
@@ -3,8 +3,8 @@ library(ggplot2)
 
 #2.1 užduotis
 
-data = readRDS("../data/692000.rds")
-png(file="../img/1_Graph.png", width = 1500, height = 1000)
-Histo = hist(data$avgWage, breaks = 200, main = paste("Vidutinio atlyginimo histograma"),
-             xlab = "Atlyginimas", ylab = "Kiekis", col = 'green')
-dev.off()
+graph_1 <- ggplot(data = data_clean, aes(x = avgWage)) +
+  geom_histogram() +
+  labs(y = "Kiekis", x = "Vidutinis atlyginimas", main = "Vidutinio atlyginimo histograma")
+
+ggsave("../img/1_uzd.png", graph_1, width = 3000, height = 1500, units = ("px"))
