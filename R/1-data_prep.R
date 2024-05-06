3550cc9ab9b6be31bb86cc515b127118b93a3748
Ugne-Kor <ugne.korsakaite@ktu.edu>
2024-05-06 22:34:51 +0300

    pakeitimo aprasymas

--- R/1-data_prep.R
+++ R/1-data_prep.R
@@ -1,19 +1,16 @@
-install.packages("tidyverse")
-install.packages("rjson")
-install.packages("jsonlite")
-
 library(tidyverse)
-library(rjson)
 library(jsonlite)
-
 cat("Darbinė direktorija:", getwd())
-download.file("https://atvira.sodra.lt/imones/downloads/2023/monthly-2023.csv.zip", "../data/temp")
-unzip("../data/temp",  exdir = "../data/")
 
-data <- fromJSON("../data/monthly-2023.json")
+
+download.file("https://atvira.sodra.lt/imones/downloads/2023/monthly-2023.json.zip", "../data/temp")
+unzip("../data/temp",  exdir = "../data/")
+readLines("../data/monthly-2023.json", 20)
+data = fromJSON("../data/monthly-2023.json")
 data =data %>%
   filter(`ecoActCode` == '522920') %>%
   saveRDS("../data/duomenys.rds")
 
 file.remove("../data/temp")
-file.remove("../data/monthly-2023.csv")
+#file.remove("../data/monthly-2023.json")
+
